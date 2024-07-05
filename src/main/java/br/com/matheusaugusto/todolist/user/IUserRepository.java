602ff873d0511f96bad529bc513613257ff0ca63
Matheus Augusto <matheus.a8@hotmail.com>
2024-07-04 23:24:51 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/matheusaugusto/todolist/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.matheusaugusto.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+    
+}
---
+++ src/test/java/br/com/matheusaugusto/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusaugusto.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
