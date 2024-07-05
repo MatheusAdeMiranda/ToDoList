4a6746c61bc1372f649ec09778703bfd5c702796
Matheus Augusto <matheus.a8@hotmail.com>
2024-07-05 13:41:29 -0300

    Dockerfile deply

--- src/main/java/br/com/matheusaugusto/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/matheusaugusto/todolist/filter/FilterTaskAuth.java
@@ -27,7 +27,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
                 var servletPath = request.getServletPath();
 
-                if(servletPath.equals("/tasks/")) {
+                if(servletPath.startsWith("/tasks/")) {
                      // Pegar a autenticação (usuario e senha)
                 var authorization = request.getHeader("Authorization");
                 
@@ -50,12 +50,13 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                         // Validar senha
                         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                         if(passwordVerify.verified) {
+                            // Continua
+                            request.setAttribute("idUser", user.getId());
                             filterChain.doFilter(request, response);
                         } else {
                             response.sendError(401);
                         }
-                        // Continua
-                     
+                             
             }
          }else {
             filterChain.doFilter(request, response);
--- src/main/java/br/com/matheusaugusto/todolist/user/UserModel.java
+++ src/main/java/br/com/matheusaugusto/todolist/user/UserModel.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 
 @Entity(name ="tb_users")
 public class UserModel {
